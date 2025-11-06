10d3cebb8596e187ee25cf97c78f7e50fc1fe56f
Simon Muriu <muriusimon6@gmail.com>
2025-11-06 11:46:57 +0300

    Add basic_operations.py to correct directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+# Addition
+sum_result = number1 + number2
+print("Addition of 10 and 15 is", sum_result)
+
+# Subtraction
+difference_result = number1 - number2
+print("Subtraction of 10 and 15 is", difference_result)
+
+# Multiplication
+product_result = number1 * number2
+print("Multiplication of 10 and 15 is", product_result)

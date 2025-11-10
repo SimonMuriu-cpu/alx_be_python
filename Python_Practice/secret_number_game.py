8e4fe1e88b8da54cd078e277a211c19a33a8e43a
Simon Muriu <muriusimon6@gmail.com>
2025-11-10 09:58:02 +0300

    Python control flow/match case practice

---
+++ Python_Practice/secret_number_game.py
@@ -0,0 +1,23 @@
+import random
+while True:
+  secret_number = random.randint(1, 10)
+
+  guess = int(input("Guess a number between 1 and 10: "))
+
+
+  match guess:
+    case _ if guess == secret_number:
+      print("Congratulations, you guessed it!")
+
+    case _ if guess > secret_number:
+      print("Oops, your guess is a bit high. Try again!")
+
+    case _ if guess < secret_number:
+      print("Nope, your guess is a bit low. Give it another shot!")
+
+  play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+  if play_again == 'yes':
+      print("Great! Let's try again!...")
+  else:
+      print("Thanks for playing! Goodbye.")
+      break

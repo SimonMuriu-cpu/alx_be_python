c917cbeccb494b13dac212aaa2c8164fe864e116
Simon Muriu <muriusimon6@gmail.com>
2025-11-12 14:06:36 +0300

    Fixing errors

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-current_weather = str(input("What's the weather like today? (sunny/rainy/cold): "))
+current_weather = str(input("What's the weather like today? (sunny/rainy/cold): ")).strip().lower()
 if current_weather == "sunny":
   print("Wear a t-shirt and sunglasses")
 

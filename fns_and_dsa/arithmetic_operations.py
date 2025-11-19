96300bca823b97efe9b50e617c951fe4d4773742
Simon Muriu <muriusimon6@gmail.com>
2025-11-19 16:48:27 +0300

    Adding the divide by zero scenario

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,8 @@ def perform_operation(num1, num2, operation):
         return num1*num2
     
     elif operation == 'divide':
+        if num2 == 0:
+            return 'Error: Division by zero'
         return num1/num2
     
     else:

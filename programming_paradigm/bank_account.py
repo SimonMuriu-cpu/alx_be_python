eef713c56ce6dcb9d7076611111476867056e7db
Simon Muriu <muriusimon6@gmail.com>
2025-11-30 13:16:51 +0300

    Fixing display_amount errors

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,6 @@
 class BankAccount:
-  def __init__(self, account_balance, initial_balance=0):
-    self.account_balance = account_balance
-    self.initial_balance = initial_balance
+  def __init__(self, initial_balance=0):
+    self.account_balance = initial_balance
 
   def deposit(self, amount):
     if amount > 0:
